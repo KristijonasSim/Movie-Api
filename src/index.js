0c62233dc9c5a352fee80fdd44b15b258735a9b8
Kristijonas Simsonas <ksimsonas@gmail.com>
2023-01-07 14:33:12 +0200

    feat: create route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App/>} />
+    </Routes>
+  </BrowserRouter>
 );
 
