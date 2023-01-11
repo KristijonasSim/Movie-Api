389d3e3fdfa8d46f3b990275da3b40f943e469e5
Kristijonas Simsonas <ksimsonas@gmail.com>
2023-01-11 13:28:06 +0200

    feat: login errors enable

--- src/components/hooks/useLogin.js
+++ src/components/hooks/useLogin.js
@@ -1,24 +1,27 @@
 import { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
 import { userApi } from '../api/userApi'
-import { useContext } from 'react'
-import { AuthContext } from '../context/AuthContext'
+
+
 const UseLogin = () => {
 
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
   const [errorMsg, setErrorMsg] = useState(null)
   const { dispatch } = useAuthContext()
-  const { user } = useContext(AuthContext)
+
+
 
   const checkAuthorization = (loginData, mockedUsersData) => {
 
     const checkValues = mockedUsersData.find(person => person.email === loginData.email && person.password === loginData.password)
     if (checkValues){
       dispatch({ type: 'LOGIN', payload: loginData })
+    }else{
+      setError(true)
+      setIsLoading(false)
+      setErrorMsg('Email or / and password is incorect')
     }
-    console.log('gaidys', checkValues)
-
   }
 
   const login = async (values) => {
@@ -30,13 +33,7 @@ const UseLogin = () => {
         checkAuthorization(values, data)
         setIsLoading(false)
       }
-      )
-      .catch(err => {
-        setError(err)
-        setIsLoading(false)
-        setErrorMsg('Email or / and password is incorect')
-      }
-      )
+    )
   }
 
   return { login, isLoading, error, errorMsg }
